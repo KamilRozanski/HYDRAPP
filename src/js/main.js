076ae7124d1c8d08fd3983ee7e587224be918260
Kamil Rozanski <motomc1@gmail.com>
2019-08-16 22:43:38 +0100

    you can add and remove glasses

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -17,6 +17,24 @@ if ('serviceWorker' in navigator) {
 // place your code below
 
 
-console.log(`Hello world!`);
+const number = document.querySelector('.app__number--js');
+const addGlass = document.querySelector('.add__glass--js');
+const removeGlass = document.querySelector('.remove__glass--js');
+const key = new Date().toISOString().slice(0, 10);
 
 
+
+addGlass.addEventListener('click', () => {
+  if (number.innerHTML <= 9) {
+    number.innerHTML++
+  }
+})
+
+removeGlass.addEventListener('click', () => {
+if (number.innerHTML <= 10 && number.innerHTML > 0) {
+  number.innerHTML--
+}
+})
+
+
+console.log(number.innerHTML);
